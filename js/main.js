b76ad251ee72e536aa27ce09b1d19699f2fec7c4
sne <snezhana.syngur@gmail.com>
2019-09-22 23:01:04 +0200

    Подготавливает тестовые данные

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var TYPES_OF_ACCOMODATION = ['palace', 'flat', 'house', 'bungalo'];
+var CHECK_IN = ['12:00', '13:00', '14:00'];
+var CHECK_OUT = ['12:00', '13:00', '14:00'];
+
+
+var similarObjects = [];
+
+var generateSimilarObject = function () {
+
+};
+
+generateSimilarObject();
